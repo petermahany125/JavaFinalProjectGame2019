cca6346e95a57f17044b02b6def2048e9f419b53
Rajiv Srinath <rajivrondo9@gmail.com>
2019-06-10 20:05:59 -0400

    yeet

--- TestBall.java
+++ TestBall.java
@@ -1,4 +1,9 @@
+//Rajiv Srinath
+//Ball
 
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
 /**
  * Write a description of class TestBall here.
  *
@@ -7,27 +12,9 @@
  */
 public class TestBall
 {
-    // instance variables - replace the example below with your own
     private int x;
-
-    /**
-     * Constructor for objects of class TestBall
-     */
-    public TestBall()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    private int y;
+    private int x2;
+    private int y2;
+    
 }
